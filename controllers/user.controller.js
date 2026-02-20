d3252f0b4dc0f89de0b1d013e2684f617bd09692
AntonioIonica <antonioionica@gmail.com>
2026-02-20 20:50:35 +0200

    feat: create users by admin roles

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,13 @@
 import User from "../models/user.model.js";
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET, JWT_EXPIRES_IN } from "../config/env.js";
 
 export const getUsers = async (req, res, next) => {
+  // Available only to admin roles
   try {
+    // Fetch all users
     const users = await User.find();
 
     res.status(200).json({
@@ -33,7 +39,51 @@ export const getUser = async (req, res, next) => {
   }
 };
 
-export const createUser = async (req, res, next) => {};
+export const createUser = async (req, res, next) => {
+  // Available only to admin roles
+
+  // Atomic operations constraint
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const error = new Error(
+        "There is already an user created with this email. Please try a different email!",
+      );
+      error.statusCode = 409;
+      throw error;
+    }
+
+    // Hashing the password before storing it
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    // Will create a list (and return a list) of users
+    // Session is attached so can be intrerrupted in case of any issues (atomic operation)
+    const newUsers = await User.create(
+      [{ name, email, password: hashPassword, role: "user" }],
+      { session },
+    );
+
+    // Commit everything and then close the connection
+    await session.commitTransaction();
+    session.endSession();
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully!",
+      data: newUsers[0],
+    });
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+
+    next(error);
+  }
+};
 
 export const updateUser = async (req, res, next) => {};
 
