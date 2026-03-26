a6c7acc7862641ece4c03e777c9125263d9ace9b
AntonioIonica <antonioionica@gmail.com>
2026-03-27 00:46:13 +0200

    reverted to localStorage token

---
+++ client/components/DashboardClient.tsx
@@ -0,0 +1,245 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Bell, Calendar, CreditCard, TrendingUp } from "lucide-react";
+import { useEffect } from "react";
+import { useSubscriptionStore } from "@/store/useSubscriptionsStore";
+import { UserType } from "./DashboardLayoutClient";
+
+const upcomingSubscriptions = [
+  {
+    name: "Netflix",
+    description: "Netflix family plan",
+    price: 15.99,
+    currency: "USD",
+    startDate: "Mar 01, 2026",
+    renewalDate: "Mar 30 2026",
+    frequency: "monthly",
+    daysUntil: 20,
+    category: "entertainment",
+    status: "active",
+    paymentMethod: "Credit card",
+  },
+  {
+    name: "HBO max",
+    description: "Hbo premium",
+    price: 12.99,
+    currency: "USD",
+    startDate: "Mar 05, 2026",
+    renewalDate: "Apr 04 2026",
+    frequency: "monthly",
+    daysUntil: 25,
+    category: "entertainment",
+    status: "active",
+    paymentMethod: "Credit card",
+  },
+  {
+    name: "HBO Great max",
+    description: "Hbo premium",
+    price: 12.99,
+    currency: "USD",
+    startDate: "Mar 05, 2026",
+    renewalDate: "Apr 04 2026",
+    frequency: "monthly",
+    daysUntil: 25,
+    category: "entertainment",
+    status: "active",
+    paymentMethod: "Credit card",
+  },
+  {
+    name: "HBO Small max",
+    description: "Hbo premium",
+    price: 12.99,
+    currency: "USD",
+    startDate: "Mar 05, 2026",
+    renewalDate: "Apr 04 2026",
+    frequency: "monthly",
+    daysUntil: 25,
+    category: "entertainment",
+    status: "active",
+    paymentMethod: "Credit card",
+  },
+  {
+    name: "HBO BIg max",
+    description: "Hbo premium",
+    price: 12.99,
+    currency: "USD",
+    startDate: "Mar 05, 2026",
+    renewalDate: "Apr 04 2026",
+    frequency: "monthly",
+    daysUntil: 25,
+    category: "entertainment",
+    status: "active",
+    paymentMethod: "Credit card",
+  },
+  {
+    name: "HBO PRO max",
+    description: "Hbo premium",
+    price: 12.99,
+    currency: "USD",
+    startDate: "Mar 05, 2026",
+    renewalDate: "Apr 04 2026",
+    frequency: "monthly",
+    daysUntil: 25,
+    category: "entertainment",
+    status: "active",
+    paymentMethod: "Credit card",
+  },
+];
+
+const DashboardClient = ({ user }: { user: UserType }) => {
+  const totalMonthly = 156.97;
+  const totalYearly = totalMonthly * 12;
+  const upcomingPayments = 5;
+
+  const subscriptions = useSubscriptionStore((state) => state.subscriptions);
+  const setSubscriptions = useSubscriptionStore(
+    (state) => state.setSubscriptions,
+  );
+
+  useEffect(() => {
+    if (!user) return;
+
+    const fetchUserSubs = async () => {
+      const res = await fetch(
+        `http://localhost:5500/api/v1/subscriptions/user/${user._id}`,
+        {
+          method: "GET",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      );
+
+      const result = await res.json();
+      if (!result.success) return;
+      setSubscriptions(result.data);
+    };
+
+    fetchUserSubs();
+  }, []);
+
+  return (
+    <div className="bg-background space-y-6">
+      {/* Stats grid */}
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <Card className="bg-card border-border">
+          <CardHeader className="flex items-center justify-between pb-1">
+            <CardTitle className="text-muted-foreground text-sm font-bold">
+              Monthly spend
+            </CardTitle>
+            <CreditCard className="size-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="font-bold text-2xl">${totalMonthly.toFixed(2)}</div>
+            <div className="flex items-center font-semibold text-muted-foreground mt-2">
+              <span className="text-red-600">12% more</span>
+              <span className="ml-1">than last month</span>
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="bg-card border-border">
+          <CardHeader className="flex items-center justify-between pb-1">
+            <CardTitle className="text-muted-foreground text-sm font-bold">
+              Yearly projection
+            </CardTitle>
+            <TrendingUp className="size-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="font-bold text-2xl">${totalYearly.toFixed(2)}</div>
+            <div className="flex items-center font-semibold text-muted-foreground mt-2">
+              <span className="text-red-600">5% more</span>
+              <span className="ml-1">than last year</span>
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="bg-card border-border">
+          <CardHeader className="flex items-center justify-between pb-1">
+            <CardTitle className="text-muted-foreground text-sm font-bold">
+              Active subscriptions
+            </CardTitle>
+            <Bell className="size-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="font-bold text-2xl">
+              {subscriptions.length || 0}
+            </div>
+            <div className="font-semibold text-muted-foreground mt-2">
+              <span>Across 5 categories</span>
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="bg-card border-border">
+          <CardHeader className="flex items-center justify-between pb-1">
+            <CardTitle className="text-muted-foreground text-sm font-bold">
+              Upcoming renewals (30 days)
+            </CardTitle>
+            <Calendar className="size-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="font-bold text-2xl">${(65.433).toFixed(2)}</div>
+            <div className="flex items-center font-semibold text-muted-foreground mt-2">
+              <span>{upcomingPayments} payments scheduled</span>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+
+      {/* Main content */}
+      <div className="grid lg:grid-cols-3 md:grid-cols-1 gap-6 h-99">
+        <Card className="border-border bg-card lg:col-span-3 md:col-span-1">
+          <CardHeader>
+            <CardTitle>Upcoming renewals</CardTitle>
+            <CardDescription>
+              Your scheduled subscriptions payments
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="overflow-auto">
+            <div className="space-y-3">
+              {upcomingSubscriptions.map((sub) => (
+                <div
+                  key={sub.name}
+                  className="flex items-center justify-between rounded-lg border-border bg-accent/90 px-4 py-3"
+                >
+                  <div className="gap-4 flex items-center">
+                    <div className="text-sm font-bold w-30">{sub.name}</div>
+                    <div className="text-muted-foreground text-sm">
+                      {sub.renewalDate}
+                    </div>
+                    <div className="ml-15">"{sub.description}"</div>
+                  </div>
+                  <div className="flex items-center gap-4">
+                    <div className="mr-4 text-xs">{sub.paymentMethod}</div>
+                    {/* Diference of days from today until renewal date */}
+                    <div className="text-muted-foreground font-semibold">
+                      7 days
+                    </div>
+                    <div className="text-right">
+                      <div className="flex">
+                        {/* compute currency to $ */}
+                        <div className="font-bold">{sub.currency}</div>
+                        <div className="font-bold">{sub.price.toFixed(2)}</div>
+                      </div>
+                      <div className="text-xs text-muted-foreground">
+                        {sub.category}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardClient;
---
+++ client/components/DashboardLayoutClient.tsx
@@ -0,0 +1,252 @@
+"use client";
+
+import AddSubscriptionDialog from "@/components/AddSubscriptionDialog";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Separator } from "@/components/ui/separator";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarInset,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import {
+  Bell,
+  Calendar,
+  CreditCard,
+  LayoutDashboard,
+  LogOut,
+  PieChart,
+  Settings,
+  SettingsIcon,
+  User,
+} from "lucide-react";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+
+export type UserType = {
+  _id: String;
+  name: String;
+  email: String;
+};
+
+const mainNavItems = [
+  {
+    title: "Dashboard",
+    href: "/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Subscriptions",
+    href: "/dashboard/subscriptions",
+    icon: CreditCard,
+  },
+  {
+    title: "Calendar",
+    href: "/dashboard/calendar",
+    icon: Calendar,
+  },
+  {
+    title: "Analytics",
+    href: "/dashboard/analytics",
+    icon: PieChart,
+  },
+  {
+    title: "Notifications",
+    href: "/dashboard/notifications",
+    icon: Bell,
+  },
+];
+
+const secondaryNavItems = [
+  {
+    title: "Settings",
+    href: "/dashboard/settings",
+    icon: Settings,
+  },
+];
+
+const DashboardLayoutClient = ({
+  user,
+  children,
+}: {
+  user: UserType;
+  children: React.ReactNode;
+}) => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  // Sign-out
+  const handleLogout = async () => {
+    const res = await fetch("http://localhost:5500/api/v1/auth/sign-out", {
+      method: "POST",
+      credentials: "include",
+    });
+
+    router.push("/");
+  };
+
+  return (
+    <SidebarProvider className="bg-background">
+      <Sidebar variant="sidebar" collapsible="icon">
+        <SidebarHeader className="border-b border-sidebar-border">
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild size="lg">
+                <Link href="/" className="flex items-center justify-between">
+                  <div className="flex items-center justify-center rounded-lg size-8 bg-accent">
+                    Logo
+                  </div>
+                  <div className="leading-none">
+                    <span>Free plan</span>
+                  </div>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarHeader>
+        <SidebarContent className="flex flex-col justify-between">
+          <SidebarGroup>
+            <SidebarGroupLabel className="text-sm underline">
+              Menu
+            </SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {mainNavItems.map((item) => (
+                  <SidebarMenuItem key={item.href}>
+                    <SidebarMenuButton
+                      asChild
+                      isActive={pathname === item.href}
+                    >
+                      <Link href={item.href}>
+                        <item.icon className="size-3" />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+          <SidebarGroup>
+            <SidebarGroupLabel className="text-sm underline">
+              Settings
+            </SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {secondaryNavItems.map((item) => (
+                  <SidebarMenuItem key={item.href}>
+                    <SidebarMenuButton
+                      asChild
+                      isActive={pathname === item.href}
+                    >
+                      <Link href={item.href}>
+                        <item.icon className="size-3" />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter className="border-t border-sidebar-border">
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <SidebarMenuButton size="lg">
+                    <Avatar className="size-8">
+                      <AvatarFallback className="bg-accent text-accent-foreground">
+                        {/* User Name  */}
+                        {user?.name.substring(0, 1).toUpperCase()}
+                      </AvatarFallback>
+                    </Avatar>
+                    <div className="flex flex-col gap-1 leading-none">
+                      <span className="font-medium">{user?.name}</span>
+                      <span className="text-sm text-muted-foreground">
+                        {user?.email}
+                      </span>
+                    </div>
+                  </SidebarMenuButton>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  side="top"
+                  className="w-[--radix-dropdown-menu-trigger-width]"
+                >
+                  <DropdownMenuLabel className="font-medium text-sm">
+                    My account
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem asChild>
+                    <Link href="/dashboard/profile" className="flex">
+                      <User className="mr-2 size-4" />
+                      <span>Profile</span>
+                    </Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild>
+                    <Link href="/dashboard/settings" className="flex">
+                      <SettingsIcon className="mr-2 size-4" />
+                      <span>Settings</span>
+                    </Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem asChild className="w-full space-x-2">
+                    <Button className="flex" onClick={handleLogout}>
+                      <LogOut />
+                      <span>Logout</span>
+                    </Button>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
+      </Sidebar>
+      <SidebarInset>
+        <header
+          className="h-18.25 bg-accent border-b border-border px-6
+         flex items-center gap-4"
+        >
+          <SidebarTrigger />
+          <Separator
+            orientation="vertical"
+            className="h-19 text-accent-foreground"
+          />
+          <div className="flex items-center flex-1 justify-between">
+            <h1 className="text-lg font-semibold">
+              {mainNavItems.find((item) => pathname === item.href)?.title ||
+                secondaryNavItems.find((item) => pathname === item.href)
+                  ?.title ||
+                "Dashboard"}
+            </h1>
+            <AddSubscriptionDialog user={user} />
+          </div>
+        </header>
+        <main className="flex-1 overflow-auto p-6 bg-background">
+          {children}
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+};
+
+export default DashboardLayoutClient;
