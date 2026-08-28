01a480518157ac9f077f598806d7393e2c40b00e
AntonioIonica <antonioionica@gmail.com>
2026-08-28 18:03:36 +0300

    fix: changing messages to unauthorized error

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -6,17 +6,17 @@ import User from "../models/user.model.js";
 const authorize = async (req, res, next) => {
   try {
     // Running tests
-    if(NODE_ENV == "test") {
-      req.user = {
-        _id: "6a60510cc3b6a3d358ec44fd",
-      };
-      return next();
-    }
+    // if(NODE_ENV == "test") {
+    //   req.user = {
+    //     _id: "6a60510cc3b6a3d358ec44fd",
+    //   };
+    //   return next();
+    // }
 
     const token =
       req.cookies?.token || req.header("Authorization")?.replace("Bearer ", "");
 
-    if (!token) return res.status(401).json({ message: "Unauthorized!" });
+    if (!token) return res.status(401).json({ message: "Unauthorized! No token active!" });
 
     // 2. Verified by the server using the secret and get the userId from the token
     const decoded = jwt.verify(token, JWT_SECRET);
@@ -24,7 +24,7 @@ const authorize = async (req, res, next) => {
     // 3. Getting the user by the id from the decoded token
     const user = await User.findById(decoded.userId);
 
-    if (!user) return res.status(401).json({ message: "Unauthorized!" });
+    if (!user) return res.status(401).json({ message: "Unauthorized! No user found!" });
 
     // 4. If the user is valid, attach the user to the request being made
     req.user = user;
